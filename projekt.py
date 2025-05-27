44c449f8732b473fdd7a87bbb6fad0d7b1e2e684
Veronika Stuchlíková <83132130@fsv.cuni.cz>
2025-05-27 13:40:19 +0200

    první commit

---
+++ projekt.py
@@ -0,0 +1,10 @@
+
+import requests
+import pandas as pd
+from datetime import datetime
+import plotly.express as px
+from dateutil import parser
+import re
+from bs4 import BeautifulSoup
+import streamlit as st
+
